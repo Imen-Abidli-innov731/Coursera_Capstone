81675303706adba3de38689960d765d899ee30cc
Imen-Abidli-innov731 <71441947+Imen-Abidli-innov731@users.noreply.github.com>
2020-09-24 23:06:52 +0200

    New Pyhon notebook added

---
+++ examen-Capstone projects .py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Notebook Project 
+#This code uses Pandas ans numpy librairies to analyze and predict the best conditions to hit the road.
+
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
